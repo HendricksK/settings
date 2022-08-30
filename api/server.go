16757ddcc7958d57d4baf2ab90343a1e22060e60
hendricksakurvin <88704296+hendricksakurvin@users.noreply.github.com>
2022-08-31 00:37:58 +0200

    EOD time to grab some sleep

--- api/server.go
+++ api/server.go
@@ -23,7 +23,8 @@ func Run() {
 
 	server.Initialize(os.Getenv("DB_DRIVER"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
 
-	seed.Load(server.DB)
+	// seed.Load(server.DB)
+	// Will build a custom seeder at a later stage
 
 	server.Run(":8080")
 
